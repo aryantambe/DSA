941acae8695bea0c4198cc8c8ec2d1078a9ec641
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-04 10:42:04 +0530

    Butterfly Pattern Code in Java

--- src/Basics/ButterflyPattern.java
+++ src/Basics/ButterflyPattern.java
@@ -2,7 +2,7 @@ package Basics;
 
 public class ButterflyPattern {
     public static void main(String[] args) {
-        int n=4;
+        int n=5;
         //Butterfly Pattern
         for(int i=1;i<=4;i++){
             for(int j=1;j<=i;j++){
