d6f4ef9fdaadc7bf597ce795f46c699eb287ff8d
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-04 17:58:54 +0530

    Added Hollow Rhombus Pattern

--- src/Basics/rhombusPattern.java
+++ src/Basics/rhombusPattern.java
@@ -17,5 +17,22 @@ public class rhombusPattern {
             }
             System.out.println();
         }
+
+        //Hollow Rhombus
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=n;k++){
+                if(i==1||k==1||i==n||k==n)
+                System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
