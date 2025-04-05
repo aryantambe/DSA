7b6963bb7817f512192dea4cbc4f16300f628148
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-05 18:18:32 +0530

    Fibonacci Series

--- src/Basics/fibonacci.java
+++ src/Basics/fibonacci.java
@@ -2,24 +2,39 @@ package Basics;
 
 public class fibonacci {
     public static void main(String[] args) {
-        int n=6;
-        int b=1;
-        int a=1;
+//        int n=6;
+//        int b=1;
+//        int a=1;
+       int size=3;
+//
+//        int fib=0;
+//        if(n>=1){
+//            System.out.print(a+" ");
+//        }
+//        if(n>=2){
+//            System.out.print(b+" ");
+//        }
+//
+//        for(int i=3;i<=n;i++){
+//
+//            fib=a+b;
+//            System.out.print(fib+" ");
+//            a=b;
+//            b=fib;
+//        }
 
-        int fib=0;
-        if(n>=1){
-            System.out.print(a+" ");
+        for(int i=1;i<=size;i++){
+            for(char j='A';j<='A'+(i-1);j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
         }
-        if(n>=2){
-            System.out.print(b+" ");
+        for(char i='A';i<='A'+size;i++){
+            for(int  j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
         }
 
-        for(int i=3;i<=n;i++){
-
-            fib=a+b;
-            System.out.print(fib+" ");
-            a=b;
-            b=fib;
-        }
     }
 }
