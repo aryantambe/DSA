041c23ea922566d050062da26f30489445d313e3
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-05 19:57:53 +0530

    Modified Armstrong number fn and added optimal method to print all  the divisors for a number(square root optimization)

--- src/Basics/basicMaths.java
+++ src/Basics/basicMaths.java
@@ -1,5 +1,6 @@
 package Basics;
 
+import static java.lang.Math.sqrt;
 
 public class basicMaths {
     public static void extractionAndCounting(int num) {
@@ -23,6 +24,40 @@ public class basicMaths {
         }
         return rev;
     }
+    public static void armstrongNumber(int num){
+        int dupe=num;
+        int k=0;
+        int sum=0;
+        int count=0;
+        int temp = num;
+
+        // Count the number of digits
+        while (temp > 0) {
+            count++;
+            temp /= 10;
+        }
+        while(num>0){
+            k=num%10;
+            sum+= (int) Math.pow(k,count);
+            num=num/10;
+        }
+        if(sum==dupe){
+            System.out.println("Its a Armstrong number");
+        }else{
+            System.out.println("Not a Armstrong number");
+        }
+    }
+    //Pair divisor
+    public static void PrintDivisors(int num){
+        for(int i=1;i<Math.sqrt(num);i++){
+            if(num%i==0){
+                System.out.print(i+" ");
+                if((num/i)!=i){
+                    System.out.println(num/i+" ");
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         //Extracting digits
@@ -32,6 +67,8 @@ public class basicMaths {
         int n=12345678;
         int count=(int)(Math.log10(n)+1);
         System.out.println(count);
+        armstrongNumber(9474);
+        PrintDivisors(36);
 
 
 
