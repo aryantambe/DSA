c7ee500cdfd03e3a093e6ece439de32df9855d41
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-06 11:45:02 +0530

    Solved Geeks for geeks problem to return an array containing the lcm and gcd(hcf) of two numbers

--- src/Basics/basicMaths.java
+++ src/Basics/basicMaths.java
@@ -1,11 +1,10 @@
 package Basics;
 
 
-
 public class basicMaths {
     public static void extractionAndCounting(int num) {
         int k;
-        int count =0;
+        int count = 0;
         while (num > 0) {
             k = num % 10;
             System.out.println(k);
@@ -14,21 +13,23 @@ public class basicMaths {
         }
         System.out.println(count);
     }
-    public static int reverseNum(int num){
-        int rev=0;
+
+    public static int reverseNum(int num) {
+        int rev = 0;
         int t;
-        while(num>0){
-            t=num%10;
-            rev=(rev*10)+t;
-            num=num/10;
+        while (num > 0) {
+            t = num % 10;
+            rev = (rev * 10) + t;
+            num = num / 10;
         }
         return rev;
     }
-    public static void armstrongNumber(int num){
-        int dupe=num;
+
+    public static void armstrongNumber(int num) {
+        int dupe = num;
         int k;
-        int sum=0;
-        int count=0;
+        int sum = 0;
+        int count = 0;
         int temp = num;
 
         // Count the number of digits
@@ -36,43 +37,44 @@ public class basicMaths {
             count++;
             temp /= 10;
         }
-        while(num>0){
-            k=num%10;
-            sum+= (int) Math.pow(k,count);
-            num=num/10;
+        while (num > 0) {
+            k = num % 10;
+            sum += (int) Math.pow(k, count);
+            num = num / 10;
         }
-        if(sum==dupe){
+        if (sum == dupe) {
             System.out.println("Its a Armstrong number");
-        }else{
+        } else {
             System.out.println("Not a Armstrong number");
         }
     }
+
     //Pair divisor
-    public static void PrintDivisors(int num){
-        for(int i=1;i<Math.sqrt(num);i++){
-            if(num%i==0){
-                System.out.print(i+" ");
-                if((num/i)!=i){
-                    System.out.println(num/i+" ");
+    public static void PrintDivisors(int num) {
+        for (int i = 1; i < Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+                if ((num / i) != i) {
+                    System.out.println(num / i + " ");
                 }
             }
         }
     }
 
     //Prime number (Using optimal approach)
-    public static void primeNumber(int num){
-        int count=0;
-        for(int i=1;i<=Math.sqrt(num);i++){
-            if(num%i==0){
+    public static void primeNumber(int num) {
+        int count = 0;
+        for (int i = 1; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
                 count++;
-                if(num/i!=i){
+                if (num / i != i) {
                     count++;
                 }
             }
         }
-        if(count==2){
+        if (count == 2) {
             System.out.println("It is a prime number");
-        }else{
+        } else {
             System.out.println("It is a non-prime number");
         }
     }
@@ -80,16 +82,14 @@ public class basicMaths {
     public static void main(String[] args) {
         //Extracting digits
         extractionAndCounting(7789);
-        int soln=reverseNum(123);
+        int soln = reverseNum(123);
         System.out.println(soln);
-        int n=12345678;
-        int count=(int)(Math.log10(n)+1);
+        int n = 12345678;
+        int count = (int) (Math.log10(n) + 1);
         System.out.println(count);
         armstrongNumber(9474);
         PrintDivisors(36);
         primeNumber(11);
-
-
 
 
     }
