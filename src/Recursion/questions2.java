5b2d4806c38db440af577fc830b5c5b4da2addda
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-07 16:06:10 +0530

    Reversing an array using 2 methods (recursion)

---
+++ src/Recursion/questions2.java
@@ -0,0 +1,33 @@
+package Recursion;
+
+import static java.util.Collections.swap;
+
+public class questions2 {
+    //using 2 pointers
+    public static void reverseArray1(int[] arr, int l, int r){
+        if(l>=r) return;
+        int temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        reverseArray1(arr, l+1, r-1);
+    }
+    //A more optimised method using only one pointer instead of 2
+    public static void reverseArray2(int i,int n,int [] arr){
+        if(i>=n/2) return;
+        int temp=arr[i];
+        arr[i]=arr[n-i-1];
+        arr[n-i-1]=temp;
+        reverseArray2(i+1,n,arr);
+    }
+
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        int n=arr.length;
+        reverseArray1(arr,0,n-1);
+        reverseArray2(0,n,arr);
+        for(int element:arr){
+            System.out.print(element);
+        }
+
+    }
+}
