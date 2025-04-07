4a7391b54fcc5d8ab00e6816de74bba3b023e73a
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-07 16:06:34 +0530

    ..

--- src/Arrays/basics.java
+++ src/Arrays/basics.java
@@ -3,6 +3,23 @@ package Arrays;
 import java.util.Scanner;
 
 public class basics {
+    public static void reverseArray(int[] arr){
+        for(int i=0;i<=arr.length/2;i++){
+            int temp=arr[i];
+            arr[i]=arr[arr.length-1-i];
+            arr[arr.length-1-i]=temp;
+        }
+        for(int j=0;j< arr.length;j++){
+            System.out.print(arr[j]+" ");
+        }
+
+    }
+
+
+
+    public static void revArray(int arr []){
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter the size of the array ");
@@ -25,20 +42,22 @@ public class basics {
 //        }
 
         //2D array
-        int row = 3;
-        int column = 5;
-        int [][] matrix=new int[row][column];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                matrix[i][j]=sc.nextInt();
-            }
-        }
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();
-        }
+//        int row = 3;
+//        int column = 5;
+//        int [][] matrix=new int[row][column];
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < column; j++) {
+//                matrix[i][j]=sc.nextInt();
+//            }
+//        }
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < column; j++) {
+//                System.out.print(matrix[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+        int [] arr={1,2,3,4,5};
+        reverseArray(arr);
 
 
     }
