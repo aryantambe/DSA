b3730f16aa5d262f0c24a757b9b5e54d35d04ad2
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-09 19:12:27 +0530

    Implemented bubble sort recursive

--- src/Sorting/Bubble_Sort.java
+++ src/Sorting/Bubble_Sort.java
@@ -18,9 +18,27 @@ public class Bubble_Sort {
             }
         }
     }
+    public static void bubbleRecursive(int [] arr,int n){
+        if(n==1) return;
+        int didSwap=0;
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                didSwap=1;
+            }
+        }
+        if(didSwap==0){
+            return;
+        }
+        bubbleRecursive(arr,n-1);
+
+    }
     public static void main(String[] args) {
         int[] arr={5,4,3,2,1};
         bubbleFunction(arr);
+        bubbleRecursive(arr, arr.length);
         for(int e:arr){
             System.out.println(e);
         }
