d70c71af3485bd5bdf456cecfda42a03ab70f954
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-09 18:54:34 +0530

    Optimised bubble sort

--- src/Sorting/Bubble_Sort.java
+++ src/Sorting/Bubble_Sort.java
@@ -3,13 +3,18 @@ package Sorting;
 public class Bubble_Sort {
 
     public static void bubbleFunction(int [] arr){
+        int didSwap=0;
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    didSwap=1;
                 }
+            }
+            if(didSwap==0){
+                break;
             }
         }
     }
