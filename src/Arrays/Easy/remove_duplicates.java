fb05d296d4e6fefe5a08652883d40b6e12aa159d
Aryan Tambe <aryan.tambe04@gmail.com>
2025-04-13 15:21:20 +0530

    Added a fn that returns a list of items that are duplicated (TCS Question)

--- src/Arrays/Easy/remove_duplicates.java
+++ src/Arrays/Easy/remove_duplicates.java
@@ -1,39 +1,65 @@
 package Arrays.Easy;
+
 import java.util.*;
+
 public class remove_duplicates {
-    public static int bruteForce(int [] arr){
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<arr.length;i++){
+    public static int bruteForce(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
             set.add(arr[i]);
         }
-        int k=set.size();
-        int i=0;
-        for(int e:set){
-            arr[i]=e;
+        int k = set.size();
+        int i = 0;
+        for (int e : set) {
+            arr[i] = e;
             i++;
         }
         return k;
     }
-    public static void optimal(int [] arr){
-        int i=0;
-        for(int j=1;j<arr.length;j++){
-            if(arr[i]!=arr[j]){
-                arr[i+1]=arr[j];
+
+    public static void optimal(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                arr[i + 1] = arr[j];
                 i++;
             }
         }
-        System.out.println(i+1);
+        System.out.println(i + 1);
     }
+
+    public static ArrayList<Integer> returnDuplicates(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(arr);
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] == arr[j]) {
+                if (!list.contains(arr[j])) {
+                    list.add(arr[j]);
+                }
+            }
+            i++;
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
-        int[] arr={1,1,2,2,2,3,3,3};
-        int noOfUniqueValues= bruteForce(arr);
+        int[] arr = {1, 1, 2, 2, 2, 3, 3, 3};
+        int noOfUniqueValues = bruteForce(arr);
         System.out.println("The array after removing duplicates is- ");
-        for(int i=0;i<noOfUniqueValues;i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < noOfUniqueValues; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
-        System.out.println("the number of unique values are- "+noOfUniqueValues);
-        int[] arr1={1,1,1,1,2,2,2,2,3,3,3,3,};
+        System.out.println("the number of unique values are- " + noOfUniqueValues);
+        int[] arr1 = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,};
         optimal(arr1);
+
+        int[] arr2 = {1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 7, 8, 8};
+        ArrayList<Integer> dupes = returnDuplicates(arr2);
+        System.out.println("The numbers that are duplicated are- ");
+        for (int e : dupes) {
+            System.out.print(e);
+        }
     }
 }
