96b35834185674c6e42c6233fc6e99397572d750
Aryan <aryan.tambe04@gmail.com>
2025-07-01 17:01:11 +0530

    leetcode 26 included

--- src/Revision_After_End_Sems/remove_duplicates.java
+++ src/Revision_After_End_Sems/remove_duplicates.java
@@ -9,11 +9,22 @@ public class remove_duplicates {
         }
         return set;
      }
+     //assuming the array is sorted
+     private static int optimal_returns_length(int [] arr){
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[i]!=arr[j]){
+                arr[i+1]=arr[j];
+                i++;
+            }
+        }
+        return i+1; //this is the length of the array that contains all the unique elements
+     }
 
 
     
     public static void main(String[] args) {
-        int arr [] = {1,1,2,3,4,5,2,6};
+        int arr [] = {1,1,2,3,4,5,6,6};
         HashSet<Integer> set=new HashSet<>();
         set=setMethod(arr) ;
         int [] temp=new int[set.size()];
@@ -24,6 +35,9 @@ public class remove_duplicates {
         for(int e:temp){
             System.out.print(e+" ");
         }
+        System.out.println();
+
+        System.out.println(optimal_returns_length(arr));
 
         
     }
