e1c0dc1bd37e2fe325f8816852ad959faf011df1
Aryan <aryan.tambe04@gmail.com>
2025-07-01 17:07:42 +0530

    Leetcode 977

---
+++ src/Revision_After_End_Sems/sqared_and_sorted_array.java
@@ -0,0 +1,40 @@
+package Revision_After_End_Sems;
+
+
+//LEETCODE 977 
+
+public class sqared_and_sorted_array {
+    static class  Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = 0, right = n - 1;
+        int pos = n - 1;
+
+        while (left <= right) {
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+
+            if (leftSq > rightSq) {
+                res[pos--] = leftSq;
+                left++;
+            } else {
+                res[pos--] = rightSq;
+                right--;
+            }
+        }
+
+        return res;
+    }
+   
+}
+ public static void main(String[] args) {
+        Solution obj=new Solution();
+        int arr[]={-4,-1,2,3,};
+        int[] res=obj.sortedSquares(arr);
+        for (int i : res) {
+            System.out.print(i+" ");
+        }
+        
+    }
+}
