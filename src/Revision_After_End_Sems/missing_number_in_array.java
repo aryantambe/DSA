b57b7e2a604a5d70b940fde8e7a096895cbbbe7b
Aryan <aryan.tambe04@gmail.com>
2025-07-04 11:58:03 +0530

    hash array and sorting method

---
+++ src/Revision_After_End_Sems/missing_number_in_array.java
@@ -0,0 +1,33 @@
+package Revision_After_End_Sems;
+
+public class missing_number_in_array {
+    // contain numbers b/w 1 to n and if array is sorted
+    private static int sorting_first(int [] arr){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=i+1){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+
+
+    //using hashing
+    private static int hash(int[] arr){ //1,2,3,5
+        int hashArr[]=new int[arr.length+2];
+        for(int i=0;i<arr.length;i++){
+            hashArr[arr[i]]=1;
+        }
+        for(int i=1;i<hashArr.length-1;i++){
+            if(hashArr[i]==0) return i;
+        }
+        return 0;
+    }
+
+    
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5,6,7};
+        System.out.println(sorting_first(arr));
+        System.out.println(hash(arr));
+    }
+}
