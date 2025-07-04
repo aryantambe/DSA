0ae1d8b742033615f304c1571004f0c8f5f7121e
Aryan <aryan.tambe04@gmail.com>
2025-07-04 17:14:10 +0530

    corrected version

--- src/Revision_After_End_Sems/max_consecutive_ones.java
+++ src/Revision_After_End_Sems/max_consecutive_ones.java
@@ -7,17 +7,18 @@ public class max_consecutive_ones {
         for(int i=0;i<arr.length;i++){
             if(arr[i]==1){
                 count++;  
+                max=Math.max(count,max);
+
             }
             else{
-                max=count;
+                
                 count=0;
-                max=Math.max(count,max);
             }
         }
         return max;
     }
     public static void main(String[] args) {
-        int [] arr={1,1,0,0,1,1,1,0,1,1};
+        int [] arr={1,1,0,0,1,1,1,0,1,1,1,1,1,1};
         System.out.println(fn(arr)   );
     }
 }
