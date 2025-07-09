776eec6e5ff60bbd70f27c4492a9d4668bcf80cf
Aryan <aryan.tambe04@gmail.com>
2025-07-09 23:01:41 +0530

    swapping using xor

---
+++ src/Revision_After_End_Sems/swap_using_xor.java
@@ -0,0 +1,14 @@
+package Revision_After_End_Sems;
+
+public class swap_using_xor {
+    public static void main(String[] args) {
+        int a =10;
+        int b=20;
+        System.out.println("a:" +a +"b:"+b);
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("a:" +a +"b:"+b);
+
+    }
+}
