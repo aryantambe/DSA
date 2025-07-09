7d141f3be753c3c86dd69613c50c458cf26a0a12
Aryan <aryan.tambe04@gmail.com>
2025-07-09 23:33:57 +0530

    better appraoch (removed k loop by observation)

--- src/Revision_After_End_Sems/longest_subarray_of_length_x.java
+++ src/Revision_After_End_Sems/longest_subarray_of_length_x.java
@@ -17,10 +17,29 @@ public class longest_subarray_of_length_x {
         }
         return longestLength;
     }
+
+    private static int better(int [] arr,int x){
+        int longestLength=0;
+        for(int i=0;i<arr.length;i++){   //1,2,3,4,5
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                if(sum==x){
+                        longestLength=Math.max(longestLength, j-i+1);
+                    }
+            }
+        }
+        return longestLength;
+    }
+
+
     public static void main(String[] args) { 
-         int [] arr={1,2,3,1,1,2,1};
+         int [] arr={1,2,3,1,1,1,2,1};
          int res=bruteForce(arr,3);  
-         System.out.println(res);                                                        
+         System.out.println(res); 
+         int res1=better(arr,3); 
+         System.out.println(res1); 
+
         
     }
 }
