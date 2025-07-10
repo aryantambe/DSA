95284c3ec85f28e42ed60c5674b3aa1112c5f85f
Aryan <aryan.tambe04@gmail.com>
2025-07-10 16:04:09 +0530

    String comparison

---
+++ src/Strings/$1Basics.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class $1Basics {
+        public static void main(String[] args) {
+            String name="Aryan Tambe";
+            String name1="Aryan Tambe";
+
+            //==
+            System.out.println(name==name1);
+            System.out.println(name.equals(name1));
+
+            String a =new String("Aryan");
+            String b =new String("Aryan");
+
+            System.out.println(a==b);
+            System.out.println(a.equals(b));
+
+
+
+        }
+} 
