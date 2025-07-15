21486dc3467624c654a3764ea249d01e9610c98e
Aryan <aryan.tambe04@gmail.com>
2025-07-15 20:08:03 +0530

    optimal method using character hash array

--- src/Strings/$5Valid_anagram.java
+++ src/Strings/$5Valid_anagram.java
@@ -14,10 +14,28 @@ public class $5Valid_anagram {
       if(!Arrays.equals(str1,str2)) return false;
         return true;
     }
+
+    private static boolean isAnagramOptimal(String s, String t){
+        if(s.length()!=t.length()) return false;
+        int[] charHash=new int [26];
+        for(int i=0;i<s.length();i++){
+            charHash[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            charHash[s.charAt(i)-'a']--;
+        }
+        for(int e:charHash){
+            if(e!=0){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         String s="anagram";
         String t="margana";
         System.out.println(isAnagram(s, t));
+        System.out.println(isAnagramOptimal(s, t));
 
     }
 }
