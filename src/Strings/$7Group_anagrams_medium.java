c723227927a7253a57c4759756e150a442212a62
Aryan <aryan.tambe04@gmail.com>
2025-07-15 23:33:53 +0530

    bug fixed

--- src/Strings/$7Group_anagrams_medium.java
+++ src/Strings/$7Group_anagrams_medium.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class $7Group_anagrams_medium {
     private static List<List<String>> fn(String [] strs){
-       if(strs.length==0) return new ArrayList();
+       if(strs.length==0) return new ArrayList<>();
         HashMap<String,List<String>> map=new HashMap<>();
         for(String s:strs){
             char [] chars=s.toCharArray();
@@ -12,7 +12,7 @@ public class $7Group_anagrams_medium {
             String sorted=new String(chars);
             if(!map.containsKey(sorted)){
                 map.put(sorted,new ArrayList<>());
-            }
+            }   
             map.get(sorted).add(s);
         }
         List<List<String>> result = new ArrayList<>();
